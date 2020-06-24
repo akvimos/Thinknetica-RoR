5e6e5c06e7da0693d3f4661b174150996940f650
akvimos <info@eltarus.com>
2020-06-24 11:48:52 +0300

    Fix square.rb in Lesson 01

--- Lesson 01/ideal_weight.rb
+++ Lesson 01/ideal_weight.rb
@@ -5,7 +5,7 @@ name = gets.chomp
 print 'Введите ваш рост: '
 growth = gets.chomp.to_f
 ideal_weight = (growth - 110) * 1.15
-if ideal_weight < 0
+if ideal_weight.negative?
   puts "#{name}, ваш вес уже оптимальный!"
 else
   puts "#{name}, ваш идеальный вес = #{ideal_weight}!"
--- Lesson 01/square.rb
+++ Lesson 01/square.rb
@@ -7,11 +7,12 @@ b = gets.chomp.to_f
 print 'Введите коэффициент \'c\': '
 c = gets.chomp.to_f
 d = b**2 - 4 * a * c
+d_sqrt = Math.sqrt(d)
 if d.negative?
   puts 'Корней нет'
 elsif d.positive?
-  x1 = (-b + Math.sqrt(d)) / (2 * a)
-  x2 = (-b - Math.sqrt(d)) / (2 * a)
+  x1 = (-b + d_sqrt) / (2 * a)
+  x2 = (-b - d_sqrt) / (2 * a)
   puts "Первый корень равен: #{x1}"
   puts "Второй корень равен: #{x2}"
 else
